461b0da3fb7508dc15f4dc535a34e867cf274fd8
sheila-serafim <sheila.serafim@gmail.com>
2022-05-25 23:09:33 -0300

    incluindo exercicio3

--- primeiraSemana/aula1.go
+++ primeiraSemana/aula1.go
@@ -22,9 +22,30 @@ func exercicio2() {
 	fmt.Printf("Pressão: %d\n", pressao)
 }
 
+func exercicio3() {
+	var nome string      //retirado o 1 da frente do nome
+	var sobrenome string //estava certa
+	var idade int        //coloquei o tipo na frente do nome
+	sobrenome = "6"      //tirei o 1 na frente do sobrenome
+	//tirei os dois pontos
+	//coloquei aspas no 6
+	var licenca_para_dirigir = true //estava correta
+	var estaturaDaPessoa int        //tirei os espaços
+	quantidadeDeFilhos := 2         //estava correta
+
+	nome = "Sheila"
+	idade = 20
+	estaturaDaPessoa = 200
+
+	fmt.Printf("%s %s %v %v %d %d", nome, sobrenome, idade, licenca_para_dirigir, estaturaDaPessoa, quantidadeDeFilhos)
+}
+
 func main() {
 
 	exercicio1()
 	fmt.Println()
 	exercicio2()
+	fmt.Println()
+	exercicio3()
+	fmt.Println()
 }
