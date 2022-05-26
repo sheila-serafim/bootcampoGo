3356fe69be842093e6fe160969f3ad1d1f3a6cc0
sheila-serafim <sheila.serafim@gmail.com>
2022-05-26 17:39:41 -0300

    com exercicio 2 da tarde

--- aula1_tarde/tarde.go
+++ aula1_tarde/tarde.go
@@ -13,7 +13,22 @@ func letrasDeUmaPalavra() {
 	fmt.Println(len(palavra))
 }
 
+func emprestimo(idade int, empregado bool, tempoAtividade int, salario float32) {
+
+	if idade > 22 && empregado && tempoAtividade > 1 {
+		if salario > 100000 {
+			fmt.Println("Empréstimo concedido sem juros")
+		} else {
+			fmt.Println("Empréstimo concedido com juros.")
+		}
+	} else {
+		fmt.Println("Empréstimo não concedido.")
+	}
+}
+
 func main() {
 
 	letrasDeUmaPalavra()
+	fmt.Println()
+	emprestimo(23, true, 2, 200000)
 }
