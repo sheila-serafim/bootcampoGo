624e82e6f66171a5e3bd664ea149c2b14ac3f159
sheila-serafim <sheila.serafim@gmail.com>
2022-05-26 18:11:41 -0300

    exercicio meses

--- aula1_tarde/tarde.go
+++ aula1_tarde/tarde.go
@@ -26,9 +26,43 @@ func emprestimo(idade int, empregado bool, tempoAtividade int, salario float32) 
 	}
 }
 
+func aQueMesCorresponde(mes int) {
+
+	switch mes {
+	case 1:
+		fmt.Println("Janeiro")
+	case 2:
+		fmt.Println("Fevereiro")
+	case 3:
+		fmt.Println("Março")
+	case 4:
+		fmt.Println("Abril")
+	case 5:
+		fmt.Println("Maio")
+	case 6:
+		fmt.Println("Junho")
+	case 7:
+		fmt.Println("Julho")
+	case 8:
+		fmt.Println("Agosto")
+	case 9:
+		fmt.Println("Setembro")
+	case 10:
+		fmt.Println("Outubro")
+	case 11:
+		fmt.Println("Novembro")
+	case 12:
+		fmt.Println("Dezembro")
+	default:
+		fmt.Println("Mês não encontrado")
+	}
+}
+
 func main() {
 
 	letrasDeUmaPalavra()
 	fmt.Println()
 	emprestimo(23, true, 2, 200000)
+	fmt.Println()
+	aQueMesCorresponde(2)
 }
